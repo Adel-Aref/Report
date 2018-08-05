9aa6e13a18236e16ddbcc4fdf3e3c12cb1853c75
mariam <msamer@ipmagix.com>
2018-08-05 14:47:17 +0200

    first commit

---
+++ Report/Model/CollectionViewCellModel.swift
@@ -0,0 +1,9 @@
+//
+//  CollectionViewCellModel.swift
+//  Report
+//
+//  Created by Mariam on 8/4/18.
+//  Copyright © 2018 Mariam. All rights reserved.
+//
+
+import Foundation
---
+++ Report/View/HomeCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  HomeCollectionViewCell.swift
+//  Report
+//
+//  Created by Mariam on 8/3/18.
+//  Copyright © 2018 Mariam. All rights reserved.
+//
+
+import UIKit
+
+class HomeCollectionViewCell: UICollectionViewCell {
+    
+}
